4d7ff279eb189c37cae255f74f9da9d37944799b
abhishek <abhishekgharami1998@gmail.com>
2025-04-01 20:03:26 +0530

    changing the workflow of the user .

--- ChatServerPlayground/ChatServerPlayground/settings.py
+++ ChatServerPlayground/ChatServerPlayground/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # adding the app for personal
-    'personal.apps.PersonalConfig',
+    
 ]
 
 MIDDLEWARE = [
